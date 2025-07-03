2255d64774059a373642fcc49c238913375fb1f7
SANJAY\appus <saisanjay2605@gmail.com>
2025-07-03 16:35:39 +0530

    initialize

--- app.py
+++ app.py
@@ -16,4 +16,6 @@ def get_data():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
